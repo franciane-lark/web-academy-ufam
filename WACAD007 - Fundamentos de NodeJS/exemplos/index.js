27d1ae9e19f3ca634fd0bd2dc9845cef29da78ae
Franciane Lark <franciane.lark019@gmail.com>
2026-05-14 20:32:55 -0400

    update - diretórios

--- WACAD007 - Fundamentos de NodeJS/exemplos/index.js
+++ WACAD007 - Fundamentos de NodeJS/exemplos/index.js
@@ -1,16 +1,21 @@
+
+
 /*
-const fs = require ("fs") 
 
-console.log(1)
+const FOLDER = process.argv[2]
+console.log(FOLDER)
 
-fs.rename("2.txt", "3.txt", (err) => {
-    if (err) console.log(err)
-        else console.log("Arquivo renomeado")
-})
+const http = require('http');
+require('dotenv').config();
+const PORT = process.env.PORT ?? 3333;
+const server = http.createServer(function (req, res) {
+ res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+ res.write("Instituto de Computação");
+ res.end();
+});
+server.listen(PORT);
 
-console.log(2)
-*/
-
+--------------------------------------------------------------------
 const http = require("http")
 const { url } = require("inspector")
 
@@ -23,4 +28,15 @@ const servidor = http.createServer((req, res) => {
 //http://localhost:7777
 servidor.listen(7777)
 
+------------------------------------------------------------------
+const fs = require ("fs") 
 
+console.log(1)
+
+fs.rename("2.txt", "3.txt", (err) => {
+    if (err) console.log(err)
+        else console.log("Arquivo renomeado")
+})
+
+console.log(2)
+*/
